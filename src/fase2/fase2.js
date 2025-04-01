15574013ecd653cb3cb6e209ef945d3239d0e4e0
kaian <kaian.moura@sou.inteli.edu.br>
2025-04-01 09:44:56 -0300

    fix: teste link

--- src/fase2/fase2.js
+++ src/fase2/fase2.js
@@ -211,7 +211,7 @@ document.body.appendChild(keyPassContainer);
 // Criando o ícone do keyPass
 const keyPassIcon = document.createElement("img");
 keyPassIcon.id = "keypass-icon";
-keyPassIcon.src = "/assets/fase2/sprites/spriteChave.png";
+keyPassIcon.src = "assets/fase2/sprites/spriteChave.png";
 keyPassIcon.alt = "KeyPass";
 keyPassContainer.appendChild(keyPassIcon);
 
@@ -1215,7 +1215,7 @@ function startGame2() {
       
       // Load the projetil.js script dynamically with a callback
       const script = document.createElement('script');
-      script.src = "/src/fase2/projetil.js";
+      script.src = "src/fase2/projetil.js";
       script.onload = () => {
         // Remove loading message once script is loaded
         if (loadingMsg && loadingMsg.parentNode) {
